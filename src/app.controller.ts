cc99498cc90d0c9b4449b2d22d1271d7714c0572
Muhammad <muhammad.ahmed@dawaai.pk>
2022-07-04 18:09:46 +0500

    initial redis-cache commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { CACHE_MANAGER, Controller, Get, Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager-redis-store';
 
 @Controller()
 export class AppController {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const value = await this.cacheManager.get('key');
+    if(value)
     return 'Hello, World!';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import type { ClientOpts } from 'redis';
 import { AppController } from './app.controller';
 @Module({
-  imports: [ConfigModule.forRoot({
+  imports: [CacheModule.register<ClientOpts>({
+    store: redisStore,
+
+    // Store-specific configuration:
+    host: 'redis-13047.c13.us-east-1-3.ec2.cloud.redislabs.com',
+    auth_pass: '8nR64FkfEuBAa6scxvNpSEPjZoYl76SG',
+    port: 13047,
+  }),ConfigModule.forRoot({
     isGlobal: true
-  })],
+  }),],
   controllers: [AppController],
   providers: [],
 })
